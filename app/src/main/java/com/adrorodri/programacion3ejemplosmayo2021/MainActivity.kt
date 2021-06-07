05964d0c06d3239b0a870503696eeb9f55b85f85
Adrian Rodriguez <adrian.rodriguez@mojix.com>
2021-06-07 09:00:49 -0400

    Agregados diferentes elementos de UI

--- app/src/main/java/com/adrorodri/programacion3ejemplosmayo2021/MainActivity.kt
+++ app/src/main/java/com/adrorodri/programacion3ejemplosmayo2021/MainActivity.kt
@@ -2,10 +2,24 @@ package com.adrorodri.programacion3ejemplosmayo2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_elementospantalla)
+
+        val buttonContinuar = findViewById<Button>(R.id.buttonContinuar)
+        val textViewBlaBlaBla = findViewById<TextView>(R.id.textViewBlaBlaBla)
+        val editTextTextPersonName = findViewById<EditText>(R.id.editTextTextPersonName)
+
+        textViewBlaBlaBla.text = "CAMBIADO DESDE EL CODIGO!"
+
+        editTextTextPersonName.hint = "HINT DESDE EL CODIGO"
+
+        editTextTextPersonName.visibility = View.GONE
     }
 }
