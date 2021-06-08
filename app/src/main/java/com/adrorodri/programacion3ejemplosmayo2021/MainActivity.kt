d1e6a019ae2e037cebc5d09b9d671de245bd0d23
Adrian Rodriguez <adrian.rodriguez@mojix.com>
2021-06-08 09:19:50 -0400

    Agregados diferentes elementos de UI

--- app/src/main/java/com/adrorodri/programacion3ejemplosmayo2021/MainActivity.kt
+++ app/src/main/java/com/adrorodri/programacion3ejemplosmayo2021/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.adrorodri.programacion3ejemplosmayo2021
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +14,36 @@ class MainActivity : AppCompatActivity() {
         val buttonContinuar = findViewById<Button>(R.id.buttonContinuar)
         val textViewBlaBlaBla = findViewById<TextView>(R.id.textViewBlaBlaBla)
         val editTextTextPersonName = findViewById<EditText>(R.id.editTextTextPersonName)
+        val switchBoton = findViewById<Switch>(R.id.switchBoton)
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+
+        val buttonMostrarMensaje = findViewById<Button>(R.id.buttonMostrarMensaje)
+        buttonMostrarMensaje.setOnClickListener {
+            textViewBlaBlaBla.text = "CAMBIADO DESDE EL BOTON!"
+        }
+        textViewBlaBlaBla.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+        switchBoton.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                buttonMostrarMensaje.isEnabled = true
+            } else {
+                buttonMostrarMensaje.isEnabled = false
+            }
+        }
+
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when(checkedId) {
+                R.id.radioButton1 -> {
+                    textViewBlaBlaBla.text = "CAMBIADO DESDE EL RB 1!"
+                }
+                R.id.radioButton2 -> {
+                    textViewBlaBlaBla.text = "CAMBIADO DESDE EL RB 2!"
+                }
+            }
+        }
 
         textViewBlaBlaBla.text = "CAMBIADO DESDE EL CODIGO!"
 
